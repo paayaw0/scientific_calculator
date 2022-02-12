0a65a78839caec0d6fbbe2d651c1efcbe57431f8
Derek Owusu-Frimpong <paayaw.dev@gmail.com>
2022-02-12 12:22:02 +0000

    sanitize inputs

--- scientific_calculator.rb
+++ scientific_calculator.rb
@@ -17,29 +17,90 @@ class ScientificCalculator
 
   def evaluate(expression)
     #-> split expression into an array of operands and operators
-    expression_array = expression.split('')
+    expression = expression.split('')
+    #-> sanitize operands
+    #-> 1. check to see if operands are multiple digits or signed if so, concatenate them
+    
+    expression = sanitize_inputs(expression)
+    expression
+    
+    sub_expressions = []
+    # get expressions in brackets and store sub_expressions array
+    
+    ['/', '*', '+', '-'].each do |op|
+      while expression.include?(op)
+        op_index = expression.find_index(op)
+        
+        operand1 = expression[op_index-1]
+        operand2 = expression[op_index+1]
+        
+        result = operand1.send(op, operand2)
 
-    x = ''
-    formatted_expression = []
-    expression_array.each.with_index do |e, index|
-      if is_operand?(e) || e == '.'
-        x << e
-      elsif !is_operand?(e)
-        formatted_expression << [x, e]
-        x = ''
+        raise ZeroDivisionError if result.infinite?
+
+        expression[(op_index-1)..(op_index+1)] = result
       end
-
-      formatted_expression.flatten!
-      formatted_expression << x if index == (expression_array.length - 1)
     end
 
-    formatted_expression[0].to_f.send(formatted_expression[1], formatted_expression[2].to_f)
+    expression[0].round(3)
   end
 
   def is_operand?(e)
     Float(e)
   rescue ArgumentError
-    nil
+    false
+  end
+
+  def is_operator?(e)
+    !is_operand?(e)
+  end
+
+  def sanitize_inputs(expression)
+    sanitize_exp = []
+    x = ''
+    last_index = expression.length - 1
+
+    expression.each.with_index do |e, index|
+      next_e = expression[index != last_index ? index + 1 : index]
+      
+      if is_operand?(next_e)
+        if is_a_sign?(e, index, expression) || is_multiple_digits_or_includes_decimal?(e, next_e)
+          x << e
+          next
+        end
+
+        sanitize_exp << e
+      else
+        if e == '.' || next_e == '.'
+          x << e
+          next
+        end
+
+        x << e
+        sanitize_exp << x 
+        x = ''
+      end
+    end
+
+    sanitize_exp << x unless x.empty?
+    sanitize_exp.map! do |i| 
+      Float(i)
+    rescue ArgumentError
+      i
+    end
+  end
+
+  def is_a_sign?(e, index, expression)
+    last_index = expression.length - 1
+    prev_e = expression[index != 0 ? index - 1 : index]
+    next_e = expression[index != last_index ? index + 1 : index]
+    
+    (is_operator?(prev_e) && is_operand?(next_e) && is_operator?(e)) 
+  end
+
+  def is_multiple_digits_or_includes_decimal?(e, next_e)
+    (is_operand?(next_e) && is_operand?(e)) || next_e == '.' || e == '.'
   end
 end
-#update
+
+
--- scientific_calculator_spec.rb
+++ scientific_calculator_spec.rb
@@ -9,70 +9,75 @@ RSpec.describe ScientificCalculator do
       context 'simple addtion' do
         it '4+5' do
           expression = '4+5'
-          expect(calculator.solve(expression: expression)).to eq(eval(expression))
+          expect(calculator.solve(expression: expression)).to eq(9.0)
         end
 
         it '5.0+4.589' do
           expression = '5.0+4.589'
-          expect(calculator.solve(expression: expression)).to eq(eval(expression))
+          expect(calculator.solve(expression: expression)).to eq(9.589)
         end
 
         it '123+800' do
           expression = '123+800'
-          expect(calculator.solve(expression: expression)).to eq(eval(expression))
+          expect(calculator.solve(expression: expression)).to eq(923)
         end
       end
 
-    #   xcontext 'simple substraction' do
-    #     it '-4-8' do
-    #       expression = '-4-8'
-    #       expect(calculator.solve(expression: expression)).to eq(-12.0)
-    #     end
+      context 'simple substraction' do
+        it '-4-8+9' do
+          expression = '-4+-808/-99'
+          expect(calculator.solve(expression: expression)).to eq(4.162)
+        end
 
-    #     it '-4-10' do
-    #       expression = '-4-10'
-    #       expect(calculator.solve(expression: expression)).to eq(-14.0)
-    #     end
+         it '-4-10' do
+          expression = '-4-10'
+          expect(calculator.solve(expression: expression)).to eq(-14)
+        end
 
-    #     it '4--10' do
-    #       expression = '4--10'
-    #       expect(calculator.solve(expression: expression)).to eq(14.0)
-    #     end
+        it '4--10' do
+          expression = '4--10'
+          expect(calculator.solve(expression: expression)).to eq(14.0)
+        end
 
-    #     it '-4--10' do
-    #       expression = '-4--10'
-    #       expect(calculator.solve(expression: expression)).to eq(6.0)
-    #     end
-    #   end
+        it '-4--10' do
+          expression = '-4--10'
+          expect(calculator.solve(expression: expression)).to eq(6.0)
+        end
+      end
 
-    #   xcontext 'simple multiplication' do
-    #     it '4*10' do
-    #       expression = '4*10'
-    #       expect(calculator.solve(expression: expression)).to eq(40.0)
-    #     end
+      context 'simple multiplication' do
+        it '4*10' do
+          expression = '4*10'
+          expect(calculator.solve(expression: expression)).to eq(40.0)
+        end
 
-    #     it '-4*10' do
-    #       expression = '-4*10'
-    #       expect(calculator.solve(expression: expression)).to eq(-40.0)
-    #     end
-    #   end
+        it '-4*10' do
+          expression = '-4*10'
+          expect(calculator.solve(expression: expression)).to eq(-40.0)
+        end
+      end
 
-    #   xcontext 'simple division' do
-    #     it '4/10' do
-    #       expression = '4/10'
-    #       expect(calculator.solve(expression: expression)).to eq(0.4)
-    #     end
+      context 'simple division' do
+        it '4/10' do
+          expression = '4/10'
+          expect(calculator.solve(expression: expression)).to eq(0.4)
+        end
 
-    #     it '-4/10' do
-    #       expression = '-4/10'
-    #       expect(calculator.solve(expression: expression)).to eq(-0.4)
-    #     end
+        it '-4/10' do
+          expression = '-4/10'
+          expect(calculator.solve(expression: expression)).to eq(-0.4)
+        end
 
-    #     it '4/0' do
-    #       expression = '4/0'
-    #       expect(calculator.solve(expression: expression)).to raise_error(ZeroDivisionError)
-    #     end
-    #   end
+        it '4/0' do
+          expression = '4/0'
+          expect { calculator.solve(expression: expression) }.to raise_error(ZeroDivisionError)
+        end
+
+        it '8/2/2' do 
+          expression = '8/2/2'
+          expect(calculator.solve(expression: expression)).to eq(2.0)
+        end
+      end
     end
 
     # describe 'single bracket sets in multiple operations'
